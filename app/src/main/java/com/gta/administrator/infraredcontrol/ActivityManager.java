f262540c7d509629340db5e5846b38dd2c830dc8
314625433@qq.com <314625433@qq.com>
2016-11-20 14:05:15 +0800

    最新更新

--- app/src/main/java/com/gta/administrator/infraredcontrol/ActivityManager.java
+++ app/src/main/java/com/gta/administrator/infraredcontrol/ActivityManager.java
@@ -1,18 +1,9 @@
 package com.gta.administrator.infraredcontrol;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.gta.administrator.infraredcontrol.baidu_iot_hub.MqttRequest;
-import com.gta.administrator.infraredcontrol.bean.NetworkInterface;
-
-import dmax.dialog.SpotsDialog;
 
 /**
  * Created by yanwen on 16/10/15.
@@ -24,13 +15,9 @@ public class ActivityManager {
 
     private static Context mContext;
     private Intent intent=new Intent();
-    //    private ProgressDialog progressDialog;
-    private AlertDialog progressDialog;
 
-    private NetworkInterface networkInterface;
-    private Class jumpActivity;// 跳转到的activity
-    private String parameter;  // 跳转到新的activity所携带的参数，如果没有可以转送""空字符串
-    private NetworkConnectListener connectListener;
+    private String parameter;
+
 
     public static ActivityManager getInstance(Context mContext) {
         ActivityManager.mContext = mContext;
@@ -40,99 +27,14 @@ public class ActivityManager {
         return activityManager;
     }
 
-    private ActivityManager() {
-        connectListener = new NetworkConnectListener();
-    }
-
     public void startActivity(final Class activity,String parameter) {
-        this.jumpActivity = activity;
         this.parameter=parameter;
         Log.d(TAG, "startActivity: "+parameter);
-        // 获取MqttRequest实例
-        networkInterface = NetworkRequest.getInstance(mContext);
-        // 检查是否处于连接状态
-
         intent.setClass(mContext, activity);
         Bundle bundle = new Bundle();
         bundle.putString("parameter", parameter);
         intent.putExtras(bundle);
         Log.d(TAG, "startActivity: "+parameter);
-        if (networkInterface.isConnected()) {
-            mContext.startActivity(intent);
-        } else {
-            // 第一次需要打开连接
-            networkInterface.openConnect();
-            // 打开连接之后，监听连接过程的状态（连接成功或因网络问题失败）
-            networkInterface.setCallbackConnectListener(connectListener);
-        }
-    }
-
-    /**
-     * 显示提示状态框
-     */
-    private void progressShow() {
-        ((Activity) mContext).runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-//                progressDialog = new ProgressDialog(mContext);
-//                progressDialog.setMessage("连接中...");
-//                progressDialog.show();
-                progressDialog = new SpotsDialog(mContext);
-                progressDialog.show();
-            }
-        });
-    }
-
-    /**
-     * 销毁提示状态框
-     */
-    private void progressDismiss() {
-        ((Activity)mContext).runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (progressDialog != null) {
-//                    progressDialog.cancel();
-                    progressDialog.dismiss();
-                }
-            }
-        });
-    }
-
-    private void showMessge() {
-        ((Activity)mContext).runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(mContext, "连接失败，请检查网络", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-
-    /**
-     * 网络连接过程监听
-     */
-    private class NetworkConnectListener implements NetworkInterface.CallbackConnectListener {
-
-        @Override
-        public void onStartConn() {
-            Log.d(TAG, "onStartConn: 启动链接");
-            progressShow();//提示用户正在连接
-        }
-
-        @Override
-        public void onSuccess() {
-            Log.d(TAG, "onSuccess: 链接成功");
-            progressDismiss();
-            mContext.startActivity(intent);
-        }
-
-        @Override
-        public void onFaild() {
-            Log.d(TAG, "onFaild: 链接失败，请检查网络");
-            networkInterface.closeConnect();
-            progressDismiss();
-            showMessge();
-
-        }
+        mContext.startActivity(intent);
     }
 }
